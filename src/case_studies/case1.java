efb8c30ab9b66d683c87531b9ab84ce72bafb339
Akhil Raja <akhilraja1911@gmail.com>
2018-10-03 00:23:55 +0530

    Test Commit

---
+++ src/case_studies/case1.java
@@ -0,0 +1,28 @@
+package com;
+
+import com.Registar;
+import com.Student;
+import com.Validator;
+
+public class Registar {
+
+	public static Registar getRegister() {
+		return new Registar();
+	}
+
+	public String registerStuden(Student student) {
+
+		String admissionId = null;
+
+		System.out.println("Name: " + student.getStudentName());
+		Validator validator = Validator.getValidator();
+		validator.validateStudentDetails(student);
+
+		if (validator.validateStudentDetails(student)) {
+			admissionId = "STD001";
+		}
+
+		return admissionId;
+	}
+
+}
